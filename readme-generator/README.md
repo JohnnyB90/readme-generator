e49535cbd57f4cb43a52dc015c5802e85beb6afe
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-03-28 23:17:19 -0400

    First Commit

---
+++ readme-generator/README.md
@@ -0,0 +1,44 @@
+
+  ## Table of Contents
+  * [Author]
+  * [Project Name](#projectName)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Support](#support)
+  * [Questions](#questions)
+
+# Name of Author
+
+Johnny
+
+# Project Name
+
+Readme Generator
+
+## Description
+
+Generates a Readme once the questions are anwsered
+
+
+## Installation
+
+No installation required
+
+## Usage
+
+Follow instructions
+
+## License
+undefined
+MIT
+
+## Support
+
+johnny.bonee@ultimaterestoration.com
+
+## Questions
+
+For questions or feedback, please feel free to reach out to me at johnnyb90 on GitHub.
+  
---
+++ readme-generator/index.js
@@ -0,0 +1,70 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'myName',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'myGitHub',
+      message: 'What is your github username?'
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a brief description of your project:'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install your project?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage instructions:'
+    },
+    {
+      type: 'input',
+      name: 'License',
+      message: 'What license is the project released under?'
+    },
+    {
+      type: 'input',
+      name: 'Support',
+      message: 'How can users get support if they encounter issues or have questions?'
+    }
+  ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) throw err;
+      console.log(`README file ${fileName} has been created!`);
+    });
+  }
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+      .prompt(questions)
+      .then((answers) => {
+        const fileName = 'README.md';
+        const data = generateMarkdown(answers);
+        writeToFile(fileName, data);
+      });
+  }
+
+// Function call to initialize app
+init();
